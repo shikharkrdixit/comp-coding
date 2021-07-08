ced43df36716f949b9fa51e68fd17d987aff80d7
Shikhar Dixit <sdixit362@gmail.com>
2021-07-08 23:40:48 +0530

    updated-Searching

--- searchinsortedarray.cpp
+++ searchinsortedarray.cpp
@@ -1,9 +1,13 @@
-class Solution {
-public:
-    int search(vector<int>& vec, int que){
-    int lo = 0, hi = vec.size()-1, mid;
+#include <bits/stdc++.h>
+using namespace std;
+
+int binsearch(vector<int>& vec, int que, int n) {
+    int lo = 0, hi = n - 1, mid;
     while (lo < hi) {
         mid = lo + (hi - lo) / 2;
+        /*if (vec[mid] == que) {
+            return mid+1;
+        }*/
         if (vec[lo] < vec[mid]) {
             if (que < vec[lo] or vec[mid] < que)
                 lo = mid + 1;
@@ -17,8 +21,24 @@ public:
                 lo = mid + 1;
         }
     }
-    if (vec[lo] == que)
-    {return lo;}
-    return -1;
+    return vec[lo] == que ? (lo+1) : -1;
 }
-};
+
+int main()
+{
+    int n, q;
+    cin >> n >> q;
+    vector<int> vec(n);
+    vector<int> ans;
+    int que;
+    for (int i = 0; i < n; i++) {
+        cin >> vec[i];
+    }
+    for (int i = 0; i < q; i++) {
+        cin >> que;
+        int x = binsearch(vec,que,n);
+        cout << x << "\n";
+    }
+
+    return 0;
+}
