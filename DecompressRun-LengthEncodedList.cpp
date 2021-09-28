3d9c3428f6c5d9ffa80b7e433218a8fdf6c5c033
Shikhar Kumar Dixit <sdixit362@gmail.com>
2021-09-28 20:15:24 +0530

    DecompressRun-LengthEncodedList

--- DecompressRun-LengthEncodedList.cpp
+++ DecompressRun-LengthEncodedList.cpp
@@ -1,10 +1,10 @@
-class Solution {
+class Solution  {
 public:
     vector<int> decompressRLElist(vector<int>& nums) {
         int freq,val,n=nums.size();
         vector<int> vec;
         for(int i=0;i<n/2;i++){
-            freq=nums[2*i];
+            freq=nums[2*i]; 
             val=nums[2*i+1];
             while(freq>0){
                 vec.push_back(val);
